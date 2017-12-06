0527d2f142419e4ec64a1371f5df0d9f1a8dbd66
Dustinyschild <dyelland@gmail.com>
2017-12-06 12:58:45 -0600

    stared a test repository

---
+++ ObjectLiterals.js
@@ -0,0 +1,17 @@
+//Testing wrapping in quotes allows instaniating a fuction in an object
+
+var productView = {
+  price: 5.99,
+  quantity: 5,
+  'calculatevalue1'() {
+    return this.price * this.quantity;
+  },
+  calculatevalue2() {
+    return this.price * this.quantity;
+  }
+};
+
+console.log(productView.calculatevalue1());
+console.log(productView.calculatevalue2());
+
+//CONCLUSION: True But not necessary
